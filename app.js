f9f06d93886021903bf64f2661eb791628712387
Warren Fletcher <warren@fletch.band>
2023-03-27 13:34:11 +0200

    Setup ejs as our template engine

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.listen(3000, () => {
     console.log("Serving on Port 3000");
 })
 
 app.get('/', (req, res) => {
-    res.send("Blog Test");
+    res.render('home');
 })
